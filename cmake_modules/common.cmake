b09200737a4bc7a9f3cd9cc979c3e1e788166c2e
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2011-01-13 10:51:36 -0500

    Use FIND_PACKAGE for OpenCV and make sure its version is >= 2.1 provide hints when looking for the package

--- cmake_modules/common.cmake
+++ cmake_modules/common.cmake
@@ -2,12 +2,25 @@ SET(CMAKE_BUILD_TYPE Release) # | Debug
 SET(CMAKE_VERBOSE_MAKEFILE 1)
 SET(CMAKE_OSX_ARCHITECTURES i386 x86_64)
 
-FIND_PATH(OpenCV_DIR OpenCVConfig.cmake
-   "/opt/opencv/share/opencv"
-   "c:/OpenCV2.1"
+FIND_PACKAGE(OpenCV REQUIRED
+  HINTS
+  "/usr/local/share/opencv"
+  "/opt/opencv/share/opencv"
+  "c:/OpenCV2.1"
+  "c:/OpenCV2.2"
 )
 
-FIND_PATH(TESSERACT_DATA_DIR confsets 
+IF( NOT OpenCV_FOUND )
+  MESSAGE( SEND_ERROR "Sikuli requires OpenCV" )
+ENDIF()
+
+IF( ( ${OpenCV_VERSION_MAJOR} LESS 2 ) OR
+    ( ${OpenCV_VERSION_MAJOR} EQUAL 2 ) AND ( ${OpenCV_VERSION_MINOR} LESS 1
+) )
+  MESSAGE( SEND_ERROR "Siklui requires a recent version of OpenCV >= 2.1" )
+ENDIF()
+
+FIND_PATH(TESSERACT_DATA_DIR confsets
    "/opt/local/share/tessdata"
    "/usr/local/share/tessdata"
    "/usr/share/tesseract-ocr/tessdata"
@@ -23,7 +36,6 @@ IF(WIN32)
 ENDIF()
 
 message("Tesseract-OCR Data Path: ${TESSERACT_DATA_DIR}")
-#message("OpenCV Path: ${OpenCV_DIR}")
 
 IF(UNIX AND NOT APPLE)
    SET(LINUX 1)
